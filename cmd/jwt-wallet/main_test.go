182f87224f4d2ef0af60c0a5f6632697b64b7d37
jhorecny-figure <jhorecny@figure.com>
2021-12-21 09:22:58 -0600

    use signer to generate tokens for test, remove commented out code

--- cmd/jwt-wallet/main_test.go
+++ cmd/jwt-wallet/main_test.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
+	"encoding/hex"
 	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/Kong/go-pdk/test"
+	secp256k1 "github.com/btcsuite/btcd/btcec"
+	"github.com/golang-jwt/jwt/v4"
 	jwtwallet "github.com/provenance-io/kong-jwt-wallet"
 	"github.com/provenance-io/kong-jwt-wallet/grants"
+	"github.com/provenance-io/kong-jwt-wallet/signing"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -32,6 +38,19 @@ func init() {
 	grants.Client = &MockClient{}
 
 }
+
+func TestMissingAuthHeader(t *testing.T) {
+	env, err := test.New(t, test.Request{
+		Method: "GET",
+		Url:    "http://example.com",
+	})
+	assert.NoError(t, err)
+
+	env.DoHttp(config)
+
+	assert.Equal(t, 401, env.ClientRes.Status)
+}
+
 func TestInvalidJwt(t *testing.T) {
 	env, err := test.New(t, test.Request{
 		Method:  "GET",
@@ -46,10 +65,17 @@ func TestInvalidJwt(t *testing.T) {
 }
 
 func TestMissingAddrClaim(t *testing.T) {
+	pkBytes, _ := hex.DecodeString("8C037EFC21AB3F0F8D32CF209D90FDBF41D10071FF600BA66A30EFA994F268A3")
+	prvk, pubk := secp256k1.PrivKeyFromBytes(secp256k1.S256(), pkBytes)
+
+	claims := GenerateClaims("", pubk)
+	token := jwt.NewWithClaims(signing.NewSecp256k1Signer(), claims)
+	sig, _ := token.SignedString(prvk)
+
 	env, err := test.New(t, test.Request{
 		Method:  "GET",
 		Url:     "http://example.com",
-		Headers: map[string][]string{"Authorization": {"eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJwcm92ZW5hbmNlLmlvIiwic3ViIjoiQTQ1SndKcEptX2J1UW5vd2Z6SE9RQk5oYTRpNEQ5cUYyOUZPVnQ3NGlqQ1UiLCJpYXQiOiIxNjQwMDMzOTU2MDAwIiwiZXhwIjoiMTY0MDIxMzk1NjAwMCJ9.jKKH5C7dl_fv7MHNCYxq_CUb7ZZAMIHgkKcasfNNLIxFq1xQ-8g2FyYUPdJZbXset-0I7TCb-VrBcH8DvJZDaQ"}},
+		Headers: map[string][]string{"Authorization": {sig}},
 	})
 	assert.NoError(t, err)
 
@@ -59,20 +85,33 @@ func TestMissingAddrClaim(t *testing.T) {
 }
 
 func TestMissingSubClaim(t *testing.T) {
+	pkBytes, _ := hex.DecodeString("8C037EFC21AB3F0F8D32CF209D90FDBF41D10071FF600BA66A30EFA994F268A3")
+	prvk, _ := secp256k1.PrivKeyFromBytes(secp256k1.S256(), pkBytes)
+
+	claims := GenerateClaims("tbMadeUpAddr", nil)
+	token := jwt.NewWithClaims(signing.NewSecp256k1Signer(), claims)
+	sig, _ := token.SignedString(prvk)
+
 	env, err := test.New(t, test.Request{
 		Method:  "GET",
 		Url:     "http://example.com",
-		Headers: map[string][]string{"Authorization": {"eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJwcm92ZW5hbmNlLmlvIiwiaWF0IjoiMTY0MDAzNDQzOTAwMCIsImV4cCI6IjE2NDAyMTQ0MzkwMDAiLCJhZGRyIjoidHAxZnllZGZlZ3pndzg4cWR4NDB4ejVwcXdqaHR0NmZqNmR3NnhwcGEifQ.JEw7aDqyX5IL3IwUo7SxaNZ6syh0bao8aBXAmo4UAJoSo5bB2BldLhhO4LatfdxDV_6juyaiwjQGv8e6FjwnIA"}},
+		Headers: map[string][]string{"Authorization": {sig}},
 	})
 	assert.NoError(t, err)
 
-	config := &jwtwallet.Config{}
 	env.DoHttp(config)
 
 	assert.Equal(t, 401, env.ClientRes.Status)
 }
 
 func TestValidJwt(t *testing.T) {
+	pkBytes, _ := hex.DecodeString("8C037EFC21AB3F0F8D32CF209D90FDBF41D10071FF600BA66A30EFA994F268A3")
+	prvk, pubk := secp256k1.PrivKeyFromBytes(secp256k1.S256(), pkBytes)
+
+	claims := GenerateClaims("tbMadeUpAddr", pubk)
+	token := jwt.NewWithClaims(signing.NewSecp256k1Signer(), claims)
+	sig, _ := token.SignedString(prvk)
+
 	r := ioutil.NopCloser(bytes.NewReader([]byte(grantsJSONString)))
 	GetDoFunc = func(*http.Request) (*http.Response, error) {
 		return &http.Response{
@@ -84,7 +123,7 @@ func TestValidJwt(t *testing.T) {
 	env, err := test.New(t, test.Request{
 		Method:  "GET",
 		Url:     "http://example.com",
-		Headers: map[string][]string{"Authorization": {"eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QifQ.eyJhZGRyIjoidHAxdXo1ZzcycHZmcmRubTlxbmpweXZzbndjNjRkNHd5Z3lxYW54MnQiLCJpc3MiOiJwcm92ZW5hbmNlLmlvIiwic3ViIjoiQWtqUlJMaGtzdU5rWF9Ba2pjVlpBX01ZOVpCNEd1cEtva2RlbU9LYnFRUFEiLCJleHAiOjQwNzA5MDg4MDAsImlhdCI6MTYwOTQ1OTIwMH0.zdGle-_d5qg0iVp_2gJ7zwBkqPCiO0YXDzCF37rviu0c7eP32qcCv5NTeKttKpXqPzaIWDkqxdrwYNlaSy26xQ"}},
+		Headers: map[string][]string{"Authorization": {sig}},
 	})
 	assert.NoError(t, err)
 
@@ -93,6 +132,23 @@ func TestValidJwt(t *testing.T) {
 	assert.Equal(t, 200, env.ClientRes.Status)
 	assert.NotEmpty(t, env.ServiceReq.Headers.Get("x-roles"))
 	assert.Equal(t, xRoles, env.ServiceReq.Headers.Get("x-roles"))
+}
+
+func GenerateClaims(addr string, pubKey *secp256k1.PublicKey) *signing.Claims {
+	compressedKey := ""
+	if pubKey != nil {
+		compressedKey = base64.RawURLEncoding.EncodeToString(pubKey.SerializeCompressed())
+	}
+	loc, _ := time.LoadLocation("GMT")
+	return &signing.Claims{
+		Addr: addr,
+		RegisteredClaims: *&jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Date(2099, 1, 1, 0, 0, 0, 0, loc)),
+			IssuedAt:  jwt.NewNumericDate(time.Date(2021, 1, 1, 0, 0, 0, 0, loc)),
+			Issuer:    "provenance.io",
+			Subject:   compressedKey,
+		},
+	}
 }
 
 var grantsJSONString = `
