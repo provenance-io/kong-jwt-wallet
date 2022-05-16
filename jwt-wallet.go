bb4cd126aec6520ed14393857545564cc15969ab
Michael Yang <mkyang@figure.com>
2022-05-16 12:54:05 -0700

    fix spacing

--- jwt-wallet.go
+++ jwt-wallet.go
@@ -88,8 +88,8 @@ func (conf Config) Access(kong *pdk.PDK) {
 	}
 
 	if conf.RBAC != "" {
-        kong.ServiceRequest.AddHeader(conf.AccessHeader, string(accessJson))
-    }
+			kong.ServiceRequest.AddHeader(conf.AccessHeader, string(accessJson))
+	}
 
 	if conf.SenderHeader != "" {
 			kong.ServiceRequest.SetHeader(conf.SenderHeader, sender)
